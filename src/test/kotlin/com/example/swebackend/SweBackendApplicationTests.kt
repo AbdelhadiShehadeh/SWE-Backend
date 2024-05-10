3630ff7b71f09651aafb0113521d1b04f24f57dc
Abdelhadi Shehadeh <a.shehadeh208@gmail.com>
2024-05-10 19:04:05 +0300

    initial project setup

---
+++ src/test/kotlin/com/example/swebackend/SweBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.swebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SweBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
